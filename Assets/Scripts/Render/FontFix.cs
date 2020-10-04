1ae74c17ca9295ad697bff7292a1f64e00bf0a4d
Mat Stevenson <mat@offaxis.dev>
2020-10-04 09:23:46 -0700

    UI judder fix.

---
+++ Assets/Scripts/Render/FontFix.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FontFix : MonoBehaviour
+{
+    [SerializeField]
+    Font _font;
+
+    void Awake()
+    {
+        _font.material.mainTexture.filterMode = FilterMode.Point;
+    }
+}
